58be61c344915ea9d1ac8ac1453307bc0921ef23
albanodias <roberto.adj@gmail.com>
2022-09-08 20:23:28 -0300

    listando filmes

--- react-filmes/src/pages/Home/index.tsx
+++ react-filmes/src/pages/Home/index.tsx
@@ -1,9 +1,44 @@
-function Home(){
-  return(
-    <div>
-      <h1>BEM VINDO A HOME</h1>
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import api from "../../services/api";
+import './style.css'
+
+function Home() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "0d25ed6246b5a261bc1160d0ccc66700",
+          language: "pt-BR",
+          page: 1,
+        },
+      });
+      setFilmes(response.data.results.slice(0,10));     
+    }
+    loadFilmes();
+  }, []);
+
+  return (
+    <div className="cotainer">
+      <div className="lista-filmes">
+        {filmes.map((filme: any) => {
+          return(
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img 
+              src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+              alt={filme.title}></img>
+            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+
+
+
+              </article>
+          )})}
+      </div>     
     </div>
-  )
+  );
 }
 
 export default Home;
