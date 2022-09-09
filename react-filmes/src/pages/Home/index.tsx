dce2f58273cfdc5d28b3364fe4f8e29c6cf15efd
albanodias <roberto.adj@gmail.com>
2022-09-09 10:52:02 -0300

    loading na home e pagina de not-found

--- react-filmes/src/pages/Home/index.tsx
+++ react-filmes/src/pages/Home/index.tsx
@@ -5,6 +5,7 @@ import './style.css'
 
 function Home() {
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadFilmes() {
@@ -15,10 +16,21 @@ function Home() {
           page: 1,
         },
       });
-      setFilmes(response.data.results.slice(0,10));     
+      setFilmes(response.data.results.slice(0,10));  
+      setLoading(false);   
     }
     loadFilmes();
+
   }, []);
+
+  if(loading) {
+    return(
+      <div className="loading">
+        <h2>Loading...</h2>
+      </div>
+    )
+  }
+
 
   return (
     <div className="cotainer">
--- react-filmes/src/routes.js
+++ react-filmes/src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
+import Error from "./pages/Error";
 
 function RoutesApp() {
   return (
@@ -10,6 +11,7 @@ function RoutesApp() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/filme/:id" element={<Filme />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
